17361d98a71996e6c43ab6c0357f9c67a3270cfd
conorwell <c_wellman@coloradocollege.edu>
2026-03-07 19:01:35 -0700

    initial commit, deploy test

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: "sans-serif", padding: "40px" }}>
+      <h1>Hello from Vite + React</h1>
+      <p>If you can see this, your deployment works.</p>
+    </div>
+  )
+}
